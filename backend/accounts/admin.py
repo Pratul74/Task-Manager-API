d3d0e429efc09e0c0be17c35238d69d8a1cab8fc
Pratul Kumar <pratulkumar2003@gmail.com>
2026-04-14 22:37:42 +0530

    Added User, CustomUserManager and registered to admin

---
+++ backend/accounts/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['id', 'email', 'name', 'role', 'is_staff', 'is_active']
+    search_fields = ['email', 'name']
+    list_filter = ['role', 'is_staff', 'is_active']
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('role', 'is_staff', 'is_active', 'is_superuser')}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'role', 'is_staff', 'is_active'),
+        }),
+    )
+
+    readonly_fields = ['last_login']
+
+
+admin.site.register(User, UserAdmin)
